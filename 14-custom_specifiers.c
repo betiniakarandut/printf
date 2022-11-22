4cfc51b00aa3b7d6992746198ffad7e8569885a1
betiniakarandut <betiniakarandut@gmail.com>
2022-11-22 17:21:52 +0100

    Adding a file

--- 14-custom_specifiers.c
+++ 14-custom_specifiers.c
@@ -1,48 +1,108 @@
 #include "main.h"
 
 /**
- * rot13_fmt - Handles conversion specifier "R"
- * @buffer: Pointer to struct buffer
- * @args: the string to be converted to rot13 and printed
- *
- * Return: Pointer to struct buffer.
+ * print_bigS - Non printable characters
+ * (0 < ASCII value < 32 or >= 127) are
+ * printed this way: \x, followed by the ASCII code
+ * value in hexadecimal (upper case - always 2 characters)
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-mk_buffer rot13_fmt(mk_buffer buffer, va_list args)
+int print_bigS(va_list l, flags_t *f)
 {
-	int i;
-	char *str;
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(l, char *);
 
-	char shift13[52] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-	'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-	'l', 'm',
-	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
-	'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-	char letters[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-	'y', 'z',
-	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-	'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	(void)f;
+	if (!s)
+		return (_puts("(null)"));
 
-	str = va_arg(args, char *);
-
-	while (*str)
+	for (i = 0; s[i]; i++)
 	{
-		for (i = 0; letters[i]; i++)
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
 		{
-			if (*str == letters[i])
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
+	}
+	return (count);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @l: argument from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: length of the printed string
+ */
+int print_rev(va_list l, flags_t *f)
+{
+	int i = 0, j;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	return (i);
+}
+
+/**
+ * print_rot13 - prints a string using rot13
+ * @l: list of arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: length of the printed string
+ */
+int print_rot13(va_list l, flags_t *f)
+{
+	int i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
 			{
-				*buffer.box = shift13[i];
-				break;
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
 			}
 		}
-		if (!letters[i])
-			*buffer.box = *str;
-
-		buffer.box++;
-		buffer.size++;
-		str++;
 	}
 
-	buffer.box--;
-	return (buffer);
+	return (j);
+}
+
+/**
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
 }
